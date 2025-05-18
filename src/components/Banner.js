4ba791c5b10eddbd0a15e4cacacdac3213fd74c1
stephvvo <stephaniekvo@gmail.com>
2025-05-18 13:54:12 -0400

    Update typing speed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,10 +12,10 @@ export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [delta, setDelta] = useState(20 - Math.random() * 10);
   const [index, setIndex] = useState(1);
-  const toRotate = ["Engineering Physics Student", "FPGA Developer", "Passionate about physics, technology and semiconductors"];
-  const period = 2000;
+  const toRotate = ["Engineering Physics Graduate", "FPGA Developer"];
+  const period = 1000;
 
   useEffect(() => {
     let ticker = setInterval(() => {
