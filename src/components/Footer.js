c5a3f7ccf18300363964cb255467a1c637f5dbe0
stephvvo <stephaniekvo@gmail.com>
2025-05-18 14:18:06 -0400

    Update footer and README

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,29 @@
 import { Container, Row, Col } from "react-bootstrap";
 import logo from "../assets/img/logo.png";
-import navIcon1 from '../assets/img/nav-icon1.svg'; // e.g., LinkedIn
-import navIcon2 from '../assets/img/nav-icon2.png'; // e.g., GitHub
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.png';
+import reactLogo from '../assets/img/logo192.png';
 
 export const Footer = () => {
     return (
         <footer className="footer">
             <Container>
-                <Row className="align-item-center">
-                    <Col sm={6}>
-                        <img src={logo} alt="Logo" />
+                <Row className="align-items-center text-center text-sm-start">
+                    <Col sm={4}>
+                        <img src={logo} alt="Site Logo" />
                     </Col>
-                    <Col sm={6} className="text-center text-sm-end">
+                    <Col sm={4} className="text-center my-2">
+                        <div className="react-built">
+                            <p style={{ marginBottom: '4px' }}>Built with React</p>
+                            <img src={reactLogo} alt="React Logo" style={{ width: '30px' }} />
+                        </div>
+                    </Col>
+                    <Col sm={4} className="text-center text-sm-end">
                         <div className="social-icon">
-                            <a
-                                href="https://www.linkedin.com/in/steph-vo/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
+                            <a href="https://www.linkedin.com/in/steph-vo/" target="_blank" rel="noopener noreferrer">
                                 <img src={navIcon1} alt="LinkedIn" />
                             </a>
-                            <a
-                                href="https://github.com/stephanie-vo"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
+                            <a href="https://github.com/stephanie-vo" target="_blank" rel="noopener noreferrer">
                                 <img src={navIcon2} alt="GitHub" />
                             </a>
                         </div>
